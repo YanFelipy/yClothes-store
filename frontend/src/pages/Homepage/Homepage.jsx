30f3c4d9e4c8a79e7e613b0ced776e704a50103a
YanFelipy <yan-felipy@hotmail.com>
2026-03-21 19:33:15 -0300

    Fonts and Trending Now

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,22 +1,30 @@
 import styles from './Homepage.module.css'
 import {useNavigate} from 'react-router'
 
+// ASSETS // 
+import imgTrend from '../../assets/img/product/gym_product_trend.jpg'
+
 const Homepage = () => {
 
   let navigate = useNavigate()
   return (
     <main >
+
+   {/* SECTION NEW COLLECTIONS */}
+
         <section className={styles.container_newcollections}>
 <div className={styles.box_newArrivals}>
 <h2>Shop New Arrivals</h2>
-<button on onClick={() => 
+<button onClick={() => 
   navigate("/new-collections")
 }> Shop Now
 </button>
 </div>
         </section>
 
-        <section className={styles.container_trendings}>
+   {/* SECTION CATEGORIES */}
+
+        <section className={styles.container_categories}>
 <div className={styles.box_categories}>
 <div className={styles.kidsc}>
       <div className={styles.name_category}>
@@ -56,6 +64,99 @@ const Homepage = () => {
 </div>
 
         </section>
+
+ {/* SECTION TRENDING NOW */}
+<section className={styles.container_trendingnow}>
+
+
+
+<div className={styles.box_trending_now}>
+<h2>Trending Now</h2>
+<div className={styles.trending_products}>
+   {/* aqui vem o map do produto, mock abaixo */}
+
+<div className={styles.product_trending}>
+
+  <div className={styles.product_details}>
+
+<img src={imgTrend} className={styles.product_Image}/>
+  <h3 className={styles.product_category}>Men's</h3>
+  <p className={styles.product_name}>Gym set Black</p>
+  <p className={styles.product_price}>80,00 $</p>
+
+  </div>
+  <div className={styles.product_button}>
+    <button className={styles.btn_add}> View</button>
+    <button className={styles.btn_view}> add to cart</button>
+  </div>
+</div>
+
+<div className={styles.product_trending}>
+
+  <div className={styles.product_details}>
+
+<img src={imgTrend} className={styles.product_Image}/>
+  <h3 className={styles.product_category}>Men's</h3>
+  <p className={styles.product_name}>Gym set Black</p>
+  <p className={styles.product_price}>80,00 $</p>
+
+  </div>
+  <div className={styles.product_button}>
+    <button className={styles.btn_add}> View</button>
+    <button className={styles.btn_view}> add to cart</button>
+  </div>
+</div>
+<div className={styles.product_trending}>
+
+  <div className={styles.product_details}>
+
+<img src={imgTrend} className={styles.product_Image}/>
+  <h3 className={styles.product_category}>Men's</h3>
+  <p className={styles.product_name}>Gym set Black</p>
+  <p className={styles.product_price}>80,00 $</p>
+
+  </div>
+  <div className={styles.product_button}>
+    <button className={styles.btn_add}> View</button>
+    <button className={styles.btn_view}> add to cart</button>
+  </div>
+</div>
+<div className={styles.product_trending}>
+
+  <div className={styles.product_details}>
+
+<img src={imgTrend} className={styles.product_Image}/>
+  <h3 className={styles.product_category}>Men's</h3>
+  <p className={styles.product_name}>Gym set Black</p>
+  <p className={styles.product_price}>80,00 $</p>
+
+  </div>
+  <div className={styles.product_button}>
+    <button className={styles.btn_add}> View</button>
+    <button className={styles.btn_view}> add to cart</button>
+  </div>
+</div>
+<div className={styles.product_trending}>
+
+  <div className={styles.product_details}>
+
+<img src={imgTrend} className={styles.product_Image}/>
+  <h3 className={styles.product_category}>Men's</h3>
+  <p className={styles.product_name}>Gym set Black</p>
+  <p className={styles.product_price}>80,00 $</p>
+
+  </div>
+  <div className={styles.product_button}>
+    <button className={styles.btn_add}> View</button>
+    <button className={styles.btn_view}> add to cart</button>
+  </div>
+</div>
+
+</div>
+
+</div>
+</section>
+
     </main>
   )
 }
